6f65b3385ca345f222fca9db8cfff362d52d808b
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2022-11-11 03:04:02 +0530

    added

--- expedia_travel/src/App.js
+++ expedia_travel/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Routes/AllRoutes'
-import Stays from './Routes/Stays';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <AllRoutes /> */}
-      <Stays />
+      <AllRoutes />
     </div>
   );
 }
