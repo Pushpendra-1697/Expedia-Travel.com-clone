1a7caf32ac80e4f45940e4518cff915084658dc7
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2022-11-13 09:51:26 +0530

    added

--- expedia_travel/src/Routes/Login.jsx
+++ expedia_travel/src/Routes/Login.jsx
@@ -68,13 +68,13 @@ function Login() {
           <ModalHeader textAlign={"center"} color={"green"}>Sign in</ModalHeader>
           <ModalBody>
             <Image src="https://a.travel-assets.com/pricing-claim/sparkle_white.svg" alt=""></Image>
-            <Heading m={"5%"} as={"h6"}>Save an average of 15% on thousands of hotels when you're signed in</Heading>
+            <Heading m={"5%"} as={"h6"} fontSize="18px" fontFamily={"monospace"} color="pink.700">Save an average of 15% on thousands of hotels when you're signed in</Heading>
             <Button m={"5% 25%"} bg={"blue"}> <Icon m={"3%"} as={BsGoogle} />Sign in With Google</Button>
             <Text textAlign={"center"} m={"3% 0"}>----------or-----------</Text>
             <form onSubmit={onSubmit} ref={FormRef}>
               <div>
                 <label>
-                  <Input m={"1%"} type="email" placeholder="Email address" onChange={handleChange} />
+                  <Input m={"1%"} type="email" placeholder="Email address" onChange={handleChange} color="pink.700" />
                 </label>
               </div>
               <div>
@@ -85,9 +85,10 @@ function Login() {
                       type={show ? 'text' : 'password'}
                       placeholder='Enter password'
                       onChange={handleChange}
+                      color="pink.700"
                     />
                     <InputRightElement width='4.5rem'>
-                      <Button h='1.75rem' size='sm' onClick={handleClick}>
+                      <Button h='1.75rem' size='sm' onClick={handleClick} color="pink.700">
                         {show ? 'Hide' : 'Show'}
                       </Button>
                     </InputRightElement>
@@ -95,7 +96,7 @@ function Login() {
                 </label>
               </div>
               <div>
-                <Button m={" 0 40%"} disabled={flag === true} type="submit" onClick={() => FormRef.current.reset()}>
+                <Button m={" 0 40%"} disabled={flag === true} type="submit" onClick={() => FormRef.current.reset()} color="black" bg="green.400">
                   Sign in
                 </Button>
                 {loading && <Spinner color="red.500" size="lg" />}
@@ -104,7 +105,7 @@ function Login() {
           </ModalBody>
           <Text textAlign={"center"}>Forget password?</Text>
           <Text textAlign={"center"}>Other ways to Sign in</Text>
-          <Box display={"flex"} m={'5% 45%'} gap={'10%'}>
+          <Box display={"flex"} m={'1% 45%'} gap={'10%'}>
             <a href={"https://www.facebook.com/v11.0/dialog/oauth?response_type=code&client_id=131538103586818&response_mode=query&scope=public_profile%20email&state=HRxSJXsnuA7rBsHRoasUM_KcIoxWHmksKgR1kR_8uWQ&redirect_uri=https://www.expedia.co.in/eg-auth-svcs/api/v1/oauth2/callback/facebook-web"}> <Icon as={BsFacebook} />  </a>
             <a href="https://appleid.apple.com/auth/authorize?response_type=code&client_id=com.expedia.ul.booking.release&response_mode=form_post&scope=name%20email&state=LGtepv0n7LheC8pkJHpEAFc8_XiakwwJok0DktVBvpE&nonce=5IF7zqWMaY2ous4jnv_Rz7LaJapPwMqlTBjlGMgie9M&redirect_uri=https://www.expedia.co.in/eg-auth-svcs/api/v1/oauth2/callback/apple-web&locale=en_GB"> <Icon as={BsApple}></Icon> </a>
           </Box>
