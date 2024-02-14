d53378cb6a49c6c22ddd29698c3a4b6419847817
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2024-02-14 22:57:43 +0530

    update logo

--- expedia_travel/src/index.js
+++ expedia_travel/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import AuthContextProvider from './Context/AuthContext';
@@ -23,8 +21,3 @@ root.render(
     </ChakraProvider>
   </ThemeContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
